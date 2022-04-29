cacf02ef49ec20ac119ca1ac84cbfcba7194edd8
zhengruipeng <zrpgood@qq.com>
2022-04-29 14:56:55 +0800

    创建 Web 服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
-const greating = '你好！';
-console.log(greating);
+const http = require('http');
+const server = http.createServer(function (request, response) {
+  response.write('hello_');
+  response.end();
+});
+server.listen(3000, function () {
+  console.log('服务已启动');
+})
