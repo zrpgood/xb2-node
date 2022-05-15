f6c2272fc779b41fbab5a775463888686792f9e1
zhengruipeng <zrpgood@qq.com>
2022-05-15 11:38:52 +0800

    定义带参数的应用接口

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-const http = require('http');
-const server = http.createServer(function (request, response) {
-  response.write('hello_');
-  response.end();
-});
-server.listen(3000, function () {
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
   console.log('服务已启动');
-})
+});
+
+app.get('/', (request, response) => {
+  response.send('你好');
+});
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+  },
+  {
+    id: 2,
+    title: '望岳',
+    content: '会当凌绝顶，一览众山小'
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝'
+  }
+];
+
+app.get('/posts', (request, response) => {
+  response.send(data);
+});
+
+app.get('/posts/:postId', (request, response) => {
+  const { postId } = request.params;
+  const posts = data.filter(item => item.id == postId);
+  response.send(posts[0]);
+});
